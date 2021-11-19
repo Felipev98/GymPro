4511ef475a6c153bfa8c1b959c99242d476258b2
Felipev98 <felipe_v_spd@hotmail.com>
2021-11-19 16:26:56 -0400

    feat: option to change logos page

--- includes/personalizador-tema.php
+++ includes/personalizador-tema.php
@@ -6,29 +6,68 @@
 * @since 1.0
 */
 
-function gympro_customize_register($_wp_customize){
+function gympro_customize_register($wp_customize){
 
 // Panel encabezado
-$_wp_customize->add_panel('gympro_encabezado', array(
-    'title' => __('Encabezado', 'gympro'),
-    'description' => __('Opciones del encabezado', 'gympro'),
+$wp_customize->add_panel('gympro_encabezado', array(
+    'title' => __('Footer', 'gympro'),
+    'description' => __('Opciones del Footer', 'gympro'),
     'priority' => 35,
 ));
 // Sección footer inferior
 
-$_wp_customize->add_section('gympro_footer', array(
+$wp_customize->add_section('gympro_footer', array(
     'title' => __('Footer', 'gympro'),
     'description' => __('Opciones del footer', 'gympro'),
     'priority' => 10,
     'panel' => 'gympro_encabezado',
 ));
+$wp_customize->add_setting('gympro_settings[facebook_link]', array(
+    'default' => '',
+    'type' => 'theme_mod',
+));
+$wp_customize-> add_control('facebook_link',array(
+    'label' => __('Enlace Facebook', 'gympro'),
+    'description' => __('Escribe la URL de tu facebook', 'gympro'),
+    'section' => 'gympro_footer',
+    'settings' => 'gympro_settings[facebook_link]'));
+$wp_customize->add_setting('gympro_settings[twitter_link]', array(
+        'default' => '',
+        'type' => 'theme_mod',
+    ));
+$wp_customize-> add_control('twitter_link',array(
+        'label' => __('Enlace Twitter', 'gympro'),
+        'description' => __('Escribe la URL de tu twitter', 'gympro'),
+        'section' => 'gympro_footer',
+        'settings' => 'gympro_settings[twitter_link]'));
+
+
+        //Sección encabezado normal
+        $wp_customize->add_section('gympro_header', array(
+            'title' => __('Encabezado', 'gympro'),
+            'description' => __('Opciones del encabezado normal', 'gympro'),
+            'priority' => 20,
+            'panel' => 'gympro_encabezado',
+        ));
+// Logo
+$wp_customize->add_setting('gympro_settings[logo]', array(
+    'default' => '',
+    'type' => 'theme_mod',
+));
+$wp_customize-> add_control(new WP_customize_Image_Control($wp_customize,'logo',array(
+    'label' => __('logo', 'gympro'),
+    'description' => __('Selecciona tu logo ', 'gympro'),
+    'section' => 'gympro_footer',
+    'settings' => 'gympro_settings[logo]'
+
+)));
+
+
+
+
+
 }
 //Enlace Facebook
-// $_wp_customize->add_setting('gympro_settings[facebook_link]', array(
-//     'default' => '',
-//     'type' => 'theme_mod',
-// ));
-// $_wp_customize -> add_control('heder_facebook_link')
 
-// }
-// add_action('customize_register','gympro_customize_register');
+
+add_action('customize_register','gympro_customize_register');
